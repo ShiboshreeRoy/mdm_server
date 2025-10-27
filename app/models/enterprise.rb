a6b6d2bed19518646af04ed51548c887792e99d2
ShiboshreeRoy <shiboshreeroy169@gmail.com>
2025-10-27 21:53:08 +0600

    Update Enterprice Model

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -1,2 +1,6 @@
 class Enterprise < ApplicationRecord
+  has_many :devices
+  has_many :app_records
+  validates :provider, presence: true # 'android' or 'apple'
+  # credentials: for Android store service account json; for Apple store APNs cert info/paths (encrypted)
 end

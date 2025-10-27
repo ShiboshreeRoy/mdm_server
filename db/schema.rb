5138d44c25289cb4b67416b54a1b73edeb050e73
ShiboshreeRoy <shiboshreeroy169@gmail.com>
2025-10-27 21:52:23 +0600

    Create Db and  Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_27_155041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "app_records", force: :cascade do |t|
+    t.bigint "enterprise_id", null: false
+    t.string "package_name"
+    t.string "title"
+    t.boolean "managed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["enterprise_id"], name: "index_app_records_on_enterprise_id"
+  end
+
+  create_table "devices", force: :cascade do |t|
+    t.bigint "enterprise_id", null: false
+    t.string "device_id"
+    t.string "platform"
+    t.boolean "enrolled"
+    t.datetime "last_seen"
+    t.jsonb "metadata"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["enterprise_id"], name: "index_devices_on_enterprise_id"
+  end
+
+  create_table "enterprises", force: :cascade do |t|
+    t.string "provider"
+    t.string "name"
+    t.jsonb "credentials"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "app_records", "enterprises"
+  add_foreign_key "devices", "enterprises"
 end
